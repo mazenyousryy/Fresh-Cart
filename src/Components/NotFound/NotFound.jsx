aac3a88013a25ac7fee8dd48dbedfd020e5da690
Mazen Yousry <mazenyyousryy@gmail.com>
2024-02-24 22:44:43 +0200

    update home page path and not found page image

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ import notFoundImg from "../../images/error.svg";
 export default function NotFound() {
     return (
         <>
-            <div className="mx-auto w-50">
+            <div className="mx-auto w-50 m-5">
                 <div className="d-flex justify-content-center align-items-center">
                     <img src={notFoundImg} alt="Not Found" className="w-100" />
                 </div>
